19c0cd504556c25430fb12d653e718cc4d7c2f45
Victor Riveros <victorriba18@hotmail.com>
2024-05-31 20:39:08 -0500

    first commit

---
+++ install.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+if [[ $(sudo -n true 2>/dev/null) ]]; then
+    echo "Sudo is not required. Executing the command without sudo."
+else
+    echo "Sudo is required. Executing sudo -i."
+    sudo -i
+fi
+
+apt update
+
+apt-get install -y ca-certificates curl
+
+install -m 0755 -d /etc/apt/keyrings
+
+curl -fsSL https://download.docker.com/linux/debian/gpg -o /etc/apt/keyrings/docker.asc
+
+chmod a+r /etc/apt/keyrings/docker.asc
+
+echo "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/nullecho "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/docker.asc] https://download.docker.com/linux/debian $(. /etc/os-release && echo "$VERSION_CODENAME") stable" | tee /etc/apt/sources.list.d/docker.list > /dev/null
+
+apt update
+
+apt install -y docker-ce docker-ce-cli containerd.io docker-buildx-plugin
